0bb8ab993fe116e68eea19895a50e65e9841c412
martinko <gamato@users.sf.net>
2013-11-21 18:33:43 +0100

    skytools.stats: wip - support simple fast counters as well as custom metrics (unfinished)
    note that this really is just a brain dump / experiment in progress :-)

--- python/skytools/stats.py
+++ python/skytools/stats.py
@@ -13,15 +13,43 @@
 >>> data1 = reset_stats()
 >>> ctx.inc('count', 2)
 >>> ctx2.avg('duration', 0.6)
+>>> ctx2.set('cnt', Counter())
+>>> ctx2.inc('cnt')
+>>> ctx.set('gmean', GaugeGMean(5))
+>>> ctx.inc('gmean', 20)
+>>> ctx.inc('gmean', 270)
+>>> ctx.set('hmean', GaugeHMean(5))
+>>> ctx.inc('hmean', 10)
+>>> ctx.inc('hmean', 30)
+>>> ctx.set('wamean', GaugeWAMean())
+>>> ctx.inc('wamean', (1, 5))
+>>> ctx.inc('wamean', (2, 20))
+>>> ctx.inc('wamean', (3, 50))
+>>> ctx.set('wgmean', GaugeWGMean())
+>>> ctx.inc('wgmean', (2, 3))
+>>> ctx.inc('wgmean', (3, 2))
+>>> ctx.inc('wgmean', (4, 1))
+>>> ctx.set('whmean', GaugeWHMean())
+>>> ctx.inc('whmean', (40, 70))
+>>> ctx.inc('whmean', (25, 25))
+>>> ctx.inc('whmean', (10, 5))
+>>> ctx.set('tavg', GaugeTimedAvg(2))
+>>> time.sleep(0.2)
+>>> ctx.inc('tavg', 3)
+>>> time.sleep(0.1)
 >>> merge_stats(data1)
 >>> process_stats(True)
 {myjob.count: 4, myjob.sub.duration: 0.55}
 """
+"""
+>>> ctx.set('timer', Timer(0.2))
+"""
 
-import time
 import logging
-import urlparse
+import math
 import os.path
+import time
+import urlparse
 
 __all__ = ['get_collector', 'process_stats', 'merge_stats', 'register_sender',
            'config_stats', 'load_stats_conf']
@@ -112,6 +140,185 @@ def load_stats_conf():
     else:
         config_stats(30, 'log')
 
+#--------------------------------------
+# Metrics
+#--------------------------------------
+
+class Metric (object):
+    #def recalc (self):
+    #    raise NotImplementedError
+    def render (self):
+        pass
+    def render_text (self):
+        raise NotImplementedError
+    def render_json (self):
+        raise NotImplementedError
+    def render_netstr (self):
+        raise NotImplementedError
+    def reset (self):
+        raise NotImplementedError
+
+class Counter (Metric):
+    def __init__(self, value = 0):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+    #def recalc (self):
+    #    pass
+    def update (self, delta):
+        self.value += delta
+
+class Timer (Metric):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+    def update (self, delta):
+        self.value += delta
+
+class Gauge (Metric):
+    def __init__(self, value = None):
+        self.value = value
+    def __str__(self):
+        return str(self.eval())
+    def eval (self):
+        return self.value
+    def update (self, value):
+        self.value = value
+    def render_text (self):
+        return self.__str__()
+
+class GaugeAMean (Gauge):
+    """ arithmetic mean """
+    def __init__(self, value = None):
+        if value is None:
+            self.count = 0
+            self._temp = 0
+        else:
+            self.count = 1
+            self._temp = value
+    def update (self, value):
+        self.count += 1
+        self._temp += value
+    def merge (self, that):
+        self.count += that.count
+        self._temp += that._temp
+    def eval (self):
+        return self._temp / float(self.count)
+    #def reset (self):
+    #    self.count = self.value = 0
+
+GaugeAvg = GaugeAMean
+
+class GaugeGMean (Gauge):
+    """ geometric mean """
+    def __init__(self, value = None):
+        if value is None:
+            self.count = 0
+            self._temp = 1
+        else:
+            assert value > 0
+            self.count = 1
+            self._temp = value
+    def update (self, value):
+        assert value > 0
+        self.count += 1
+        self._temp *= value
+    def merge (self, that):
+        self.count += that.count
+        self._temp *= that._temp
+    def eval (self):
+        return math.pow (self._temp, 1./self.count)
+    #def reset (self):
+    #    self.count = self.value = 0
+
+class GaugeHMean (Gauge):
+    """ harmonic mean """
+    def __init__(self, value = None):
+        if value is None:
+            self.count = 0
+            self._temp = 0
+        else:
+            assert value > 0
+            self.count = 1
+            self._temp = 1./value
+    def update (self, value):
+        assert value > 0
+        self.count += 1
+        self._temp += 1./value
+    def merge (self, that):
+        self.count += that.count
+        self._temp += that._temp
+    def eval (self):
+        return self.count / self._temp
+
+class GaugeWAMean (Gauge):
+    """ weighted arithmetic mean """
+    def __init__(self, data = None):
+        # data = (value, weight)
+        self.data = data or (0.0, 0.0) # no effect
+        assert self.data[1] >= 0
+    def eval (self):
+        return self.data[0]
+    def update (self, data):
+        assert data[1] >= 0
+        weights = self.data[1] + data[1]
+        self.data = ((self.data[0] * self.data[1] + data[0] * data[1]) / weights, weights)
+
+class GaugeWGMean (Gauge):
+    """ weighted geometric mean """
+    def __init__(self, data = None):
+        # data = (value, weight)
+        self.data = data or (1.0, 0.0) # no effect
+        assert self.data[0] > 0 and self.data[1] >= 0
+    def eval (self):
+        return self.data[0]
+    def update (self, data):
+        assert data[0] > 0 and data[1] >= 0
+        weights = self.data[1] + data[1]
+        self.data = ((math.log(self.data[0]) * self.data[1] + math.log(data[0]) * data[1]) / weights, weights)
+
+class GaugeWHMean (Gauge):
+    """ weighted harmonic mean """
+    def __init__(self, data = None):
+        # data = (value, weight)
+        self._sum_v = self._sum_w = 0
+        if data is not None:
+            self.update(data)
+    def update (self, data):
+        assert data[0] > 0 and data[1] >= 0
+        self._sum_v += data[1] / float(data[0])
+        self._sum_w += data[1]
+    def eval (self):
+        return self._sum_w / self._sum_v
+
+class GaugeTimedAvg (GaugeWAMean):
+    """ GaugeWAMean where weight is time passed """
+    def __init__(self, value):
+        super(GaugeTimedAvg, self).__init__()
+        self._time = time.time()
+        self._cval = value
+    def update (self, value):
+        now = time.time()
+        data = (self._cval, now - self._time)
+        super(GaugeTimedAvg, self).update(data)
+        self._cval = value
+        self._time = now
+    def eval (self):
+        self.update(self._cval)
+        return self.data[0]
+
+# 2 2 2 3 3
+# 2 2 2 2.25 2.4
+
+class GaugeMin (Gauge):
+    def __init__(self, value):
+        super(GaugeMin, self).__init__(value)
+        self.min = value
+    def update(self, value):
+        if value < self.min:
+            self.min = value
+
 
 class StatsContext (object):
     """Position in namespace .
@@ -131,25 +338,31 @@ class StatsContext (object):
         """
         return StatsContext(self.prefix + name, self.delim, self.ctx)
 
-    def set(self, name, val):
+    def set(self, name, value):
         """ Set to a value.
         """
         k = self.prefix + name
-        self.ctx.data[k] = val
+        self.ctx.data[k] = value
 
-    def inc(self, name, val = 1):
+    def inc(self, name, value = 1):
         """ Add a value to be summed.
         """
         k = self.prefix + name
-        self.ctx.data[k] = val + self.ctx.data.get(k, 0)
+        try:
+            self.ctx.data[k] += value
+        except KeyError:
+            self.ctx.data[k] = value
+        except TypeError:
+            self.ctx.data[k].update(value)
 
-    def avg(self, name, val):
+    def avg(self, name, value):
         """ Add a value to be averaged.
         """
         k = self.prefix + name
-        v = self.ctx.data.setdefault(k, [0,0])
-        v[0] += val
-        v[1] += 1
+        try:
+            self.ctx.data[k].update(value)
+        except KeyError:
+            self.ctx.data[k] = GaugeAvg(value)
 
 
 def _get_context (ctx):
@@ -199,12 +412,12 @@ def merge_stats (data, context = None):
     """
     ctx = _get_context(context)
     for k, v in data.items():
-        if isinstance(v, list):
-            s = ctx.data.setdefault(k, [0,0])
-            s[0] += v[0]
-            s[1] += v[1]
-        else:
-            ctx.data[k] = v + ctx.data.get(k, 0)
+        try:
+            ctx.data[k] += v
+        except KeyError:
+            ctx.data[k] = v
+        except TypeError:
+            ctx.data[k].merge(v)
 
 
 if __name__ == '__main__':
